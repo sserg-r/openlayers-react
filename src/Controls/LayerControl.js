1086080e29255c4959c2bbd8a02e8409f5223f97
rusetskyserg <rusetskyserg@gmail.com>
2022-05-05 17:51:42 +0300

    linked layers

--- src/Controls/LayerControl.js
+++ src/Controls/LayerControl.js
@@ -1,16 +1,38 @@
 import React from "react";
+import AppContext from "../AppContext";
+import { useContext } from "react";
 
 
 const LayerControl = ({llist}) => {	
-	const lrs=llist.map((item, i) => (
-        <li key={i}>
-         <input type='checkbox'
-            checked={true} 
-            onChange={() => console.log('changed')}/>
-            {item.name} 
-            </li>             
-        
-        ))
+    const {LList, setLList} =useContext(AppContext);
+
+
+    const changeVisib = (i) => {
+        let arr=[...LList]
+        arr[i].isvisible=!arr[i].isvisible;               
+        setLList(arr)       
+    }
+    
+    var lrs=LList.map(function(item, i){ 
+        let rr=i;
+        return  <li key={i}>
+            <input type='checkbox' 
+                    checked={item.isvisible} 
+                    onChange={() => changeVisib(rr)}                
+                    />            
+            {item.name}
+        </li>})            
+
+
+	// const lrs=LList.map((item, i) => (
+    //     <li key={i}>
+    //      <input type='checkbox'
+    //         checked={true} 
+    //         onChange={() => console.log('changed')}/>
+    //         {item.name} 
+    //         </li>             
+        // 
+        // ))
         
 	return <ul>        
             {lrs}
